5d2be07bab299081e621bbdb64b5252dffe6803f
Jay Goyal <goyal.jay2003@gmail.com>
2022-02-02 17:51:13 +0530

    Clicking on element in hamburger turns if off

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hmbgr");
 const nav_cont = document.querySelector(".overlay");
+const nav_elems = document.querySelectorAll(".overlay>.nav-elem");
 hamburger.addEventListener("click", () => {
 	if (hamburger.classList.contains("hmbgr-open")) {
 		hamburger.classList.remove("hmbgr-open");
@@ -8,4 +9,13 @@ hamburger.addEventListener("click", () => {
 		hamburger.classList.add("hmbgr-open");
 		nav_cont.classList.add("overlay-open");
 	}
+});
+
+nav_elems.forEach(item => {
+	item.addEventListener("click", () => {
+		if (hamburger.classList.contains("hmbgr-open")) {
+			hamburger.classList.remove("hmbgr-open");
+			nav_cont.classList.remove("overlay-open");
+		}
+	});
 });
